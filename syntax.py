82539b596a5189252a0bab5abbeb898a6060e407
Jilian Garette Abadia Abangan <jiliangarette@gmail.com>
2025-03-05 19:24:50 -0800

    setup python django environment

---
+++ syntax.py
@@ -0,0 +1,13 @@
+def hello():
+    return "hello batungbakal!"
+
+hello()
+
+
+if len(hello()) > 5:
+    print('long text')
+elif   len(hello())  > 0:
+  print('short text')
+else:
+    print('zero text')  
+
